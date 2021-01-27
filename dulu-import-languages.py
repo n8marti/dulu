db9d2fe7613941e2080a5146151a99e41bd3a75b
Nate Marti <nate_marti@sil.org>
2021-01-27 18:27:49 +0100

    rename

--- dulu-import-languages.py
+++ dulu-import-languages.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/python3
 
 # Import language data into seed file for Dulu CAR.
 
@@ -12,17 +12,17 @@
 #Type "help" for help.
 #
 #dulu_dev=# \d languages
-# id                     | integer                     |           | not null | nextval('languages_id_seq'::regclass)
-# name                   | character varying           |           |          |
+# id                     | integer                     |           | not null |
+# name                   | character varying           |           |          | A   "Language Name"
 # category               | character varying           |           |          |
-# code                   | character varying           |           |          |
+# code                   | character varying           |           |          | B   "ISO"
 # language_status_id     | integer                     |           |          |
-# notes                  | text                        |           |          |
+# notes                  | text                        |           |          | AV  "Notes"
 # country_id             | integer                     |           |          |
 # international_language | character varying           |           |          |
-# population             | integer                     |           |          |
+# population             | integer                     |           |          | K   "No. of Speakers"
 # population_description | character varying           |           |          |
-# classification         | character varying           |           |          |
+# classification         | character varying           |           |          | L   "Lg. Family"
 # country_region_id      | integer                     |           |          |
 # created_at             | timestamp without time zone |           | not null |
 # updated_at             | timestamp without time zone |           | not null |
@@ -40,3 +40,33 @@
 #   $alt_names, $parent_id, $cluster_id, $region_id, program_id
 #);
 # ------------------------------------------------------------------------------
+
+import csv
+
+from pathlib import Path
+
+input_file = Path.home() / 'Téléchargements' / 'CAG_Lgs_Info.csv'
+
+lg_data = []
+with open(input_file, newline='') as csvfile:
+    csv_dict = csv.DictReader(csvfile, delimiter=',', quotechar='"')
+    id = 1
+    for row in csv_dict:
+        if row["Language Name"]:
+            code = row["ISO"] if row["ISO"] else None
+            notes = row["Notes"] if row["Notes"] else None
+            pop = row["No. of Speakers"] if row["No. of Speakers"] else None
+            classif = row["Lg. Family"] if row["Lg. Family"] else None
+            lg_dict = {
+                "id": id,
+                "name": row["Language Name"],
+                "code": code,
+                "notes": notes,
+                "population": pop,
+                "classification": classif,
+            }
+            lg_data.append(lg_dict)
+            id += 1
+
+for item in lg_data:
+    print(item)
