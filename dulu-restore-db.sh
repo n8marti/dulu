caa20ac9c2a3e7954f9bc44f646942ee1184b994
Nate Marti <nate_marti@sil.org>
2021-01-28 18:28:22 +0100

    fix find argument

--- dulu-restore-db.sh
+++ dulu-restore-db.sh
@@ -4,10 +4,10 @@ if [[ -e $1 ]]; then
     db_to_restore="$1"
 elif [[ $1 == 'list' ]] || [[ $1 == '--list' ]] || [[ $1 == '-l' ]]; then
     backup_dir="/home/dulu/backups/databases"
-    find "$backup_dir" -name '*.sql.gz' -maxdepth 1
+    find "$backup_dir" -maxdepth 1 -name '*.sql.gz'
     exit 0
 else
-    echo "Error: Invalid argument: $1"
+    echo "Error: Invalid or missing argument: $1"
     exit 1
 fi
 
