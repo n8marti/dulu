215767964dcef1e6d7c70749645a0cdee9fbec23
Nate Marti <nate_marti@sil.org>
2021-02-04 11:49:45 +0100

    change brackets

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,8 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'dulu-1804', user: 'dulu', roles: %w[web app db]
+#server 'dulu-1804', user: 'dulu', roles: %w[web app db]
+server 'dulu-1804', user: 'dulu', roles: %w{web app db}
 
 # role-based syntax
 # ==================
